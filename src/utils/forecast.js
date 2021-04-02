72311c6a992f360183bc7de1370562570e8384c5
Sara <saragjikdhima@gmail.com>
2021-04-02 17:13:59 +0200

    Add more data to forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degreees out. It feels like ' + body.current.feelslike + ' degrees.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degreees out. It feels like ' + body.current.feelslike + ' degrees. The humidity is about ' +body.current.humidity)
         }
     })
 }
